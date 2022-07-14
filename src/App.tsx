2271a343c88a6e6b885a0a3b64a761a12ba029aa
abc9455 <dldudwls888@gmail.com>
2022-07-14 22:55:47 +0900

    feat: type지정 Array

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 
 function App() {
   let 이름 :string | number= 'kim';
+  type Member = [number, boolean];
+  let john:Member = [123,true];
   
   return (
     <div className="App">
