4ec2af527c78832ed4212d1a0c7ca5767086d655
abc9455 <dldudwls888@gmail.com>
2022-07-15 15:36:35 +0900

    feat: 타입스크립트 자료형 타입 지정

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,27 @@ import './App.css';
 
 function App() {
   let 이름 :string | number= 'kim';
-  type Member = [number, boolean];
-  let john:Member = [123,true];
+  let 나이 :number = 20;
+  let 결혼했니 :boolean = false;
+  let 회원들 :string[] = ['kim','park'];
+  let 내정보 : {age : number} = {age : 20}
+  type Member = {
+    [key :string] : string, 
+  }
+  let john:Member = {name: 'kim', age:'123'}
+  let project :{
+    member : string[],
+    days : number,
+    started : boolean,
+  } = {
+    member : ['kim', 'park'],
+    days : 30,
+    started : true,
+  }
   
   return (
     <div className="App">
+      {john.name}
       {이름}
     </div>
   );
